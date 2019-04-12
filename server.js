11158987ffdab98b40107d4a2e366e1d59a87cc8
Antonio Stacy Foushee <AntonioStacy@gmail.com>
2019-04-12 01:26:34 -0400

    still works too

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/login', function(req, res) {
     querystring.stringify({
       response_type: 'code',
       client_id: process.env.SPOTIFY_CLIENT_ID,
-      scope: 'user-read-private user-read-email',
+      scope: 'user-read-private user-read-email user-follow-read',
       redirect_uri
     }))
 })
@@ -36,7 +36,7 @@ app.get('/callback', function(req, res) {
   }
   request.post(authOptions, function(error, response, body) {
     var access_token = body.access_token
-    let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
+    let uri = process.env.FRONTEND_URI || 'https://bandmap-frontend.herokuapp.com/'
     res.redirect(uri + '?access_token=' + access_token)
   })
 })
